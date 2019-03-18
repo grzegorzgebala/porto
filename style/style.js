1e1ae797e99dd60378b5ba93fc3342e2d5f77357
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2019-03-18 01:05:18 +0100

    Content changed

--- style/style.js
+++ style/style.js
@@ -1,36 +1,51 @@
 'use strict';
 
+// menu header
 function goToSection1() {
     document.getElementById("section1").scrollIntoView();
-    for (let i=1; i<7; i++) {
+    for (let i=1; i<=6; i++) {
         document.getElementById(i).classList.remove("change-color");
     }
 }
 
 function goToSection2() {
     document.getElementById("section2").scrollIntoView();
-    for (let i=1; i<7; i++) {
+    for (let i=1; i<=6; i++) {
         document.getElementById(i).classList.add("change-color");
     }
 }
 
 function goToSection3() {
     document.getElementById("section3").scrollIntoView();
-    for (let i=1; i<7; i++) {
+    for (let i=1; i<=6; i++) {
         document.getElementById(i).classList.add("change-color");
     }
 }
 
 function goToSection4() {
     document.getElementById("section4").scrollIntoView();
-    for (let i=1; i<7; i++) {
+    for (let i=1; i<=6; i++) {
+        document.getElementById(i).classList.add("change-color");
+    }
+}
+function goToSection5() {
+    document.getElementById("section5").scrollIntoView();
+    for (let i=1; i<=6; i++) {
         document.getElementById(i).classList.add("change-color");
     }
 }
 
-function goToSection5() {
-    document.getElementById("section5").scrollIntoView();
-    for (let i=1; i<7; i++) {
-        document.getElementById(i).classList.add("change-color");
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 853) {
+        for (let i=1; i<=6; i++) {
+            document.getElementById(i).classList.add("change-color");
+        }
+    } else {
+        for (let i=1; i<=6; i++) {
+            document.getElementById(i).classList.remove("change-color");
+        }
+        
+
     }
-}
+});
